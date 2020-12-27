e002fb6a485b797ca5dac1a10e5f6dea84173e6c
Andrean Tama <andreanyogatama1@gmail.com>
2020-12-27 21:56:19 +0700

    add : map, reduce, filter for array

--- simpleproject/arraytype.ts
+++ simpleproject/arraytype.ts
@@ -14,7 +14,24 @@ var person: {
  let exampleArrayMix: any[];
  exampleArrayMix = ["1", 2, "3"];
 
+ let randomNumber: number[];
+ randomNumber = [1, 2, 4, 6.6];
+
  console.log(person.name);
  for(const data of person.hobby){
      console.log(data);
  }
+let isNumberTwo = exampleArrayString.filter((value: string) => {
+    return (value == "2")
+});
+console.log(isNumberTwo);
+
+let totalNumber = randomNumber.reduce((total: number, value: number) => {
+    return (total + value);
+}, 0);
+console.log(totalNumber);
+
+let multipleNumber = randomNumber.map((value: number) => {
+    return (value * 2);
+});
+console.log(multipleNumber);
