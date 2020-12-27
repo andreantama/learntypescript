08577e3fd06b2060be96e624ce1223a28fdb5e87
Andrean Tama <andreanyogatama1@gmail.com>
2020-12-27 22:04:23 +0700

    create : tuplestype (fixed array) in typesript

---
+++ simpleproject/tuplestype.ts
@@ -0,0 +1,12 @@
+//Tuples is similiar like array, but with tumples we can excat elements of array
+var person: {
+    name: string;
+    age: number;
+    hobby: string[];
+    role: [number, string];
+}={
+    name: "Andrean Yogatama",
+    age: 24,
+    hobby:["Bike", "Coding"],
+    role: [2, "superadmin"]
+ }
