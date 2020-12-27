362bb358a4e7c7fb035b0063006b76a2e800747a
Andrean Tama <andreanyogatama1@gmail.com>
2020-12-27 21:18:19 +0700

    create : example datatype with typescript

--- simpleproject/app.js
+++ simpleproject/app.js
@@ -1,1 +1,14 @@
-console.log("app here");
+function add(a, b, pharse, showresult) {
+    var result = (a + b);
+    if (showresult == true) {
+        console.log(pharse + result);
+    }
+    else {
+        return result;
+    }
+}
+var a = 50;
+var b = 100;
+var pharse = "Result is ";
+var showresult = false;
+add(a, b, pharse, showresult);
